caed17d8901af7e2809d1e39a75ecb4cebb72cda
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2023-02-23 09:51:41 -1000

    Move tql to this repo

---
+++ tql/tql.test.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+const path = require("path")
+const { TestRacer } = require("jtree/products/TestRacer.js")
+const { TrueBaseFolder } = require("../code/TrueBase.js")
+const tql = require("./tql.nodejs.js")
+
+const baseFolder = path.join(__dirname, "..", "planetsDB")
+const folder = new TrueBaseFolder()
+  .setDir(baseFolder)
+  .setGrammarDir(baseFolder)
+  .loadFolder()
+
+const testTree = {}
+
+testTree.all = equal => {
+  // Arrange
+  const program = new tql(`includes mars
+doesNotInclude zzzzz
+matchesRegex \\d+
+where moons = 1
+where diameter > 10000
+where related includes mars
+notMissing diameter
+rename diameter Diameter`)
+
+  // Act/Assert
+  const results = program.filterFolder(folder)
+  equal(results.length, 1)
+}
+
+if (!module.parent) TestRacer.testSingleFile(__filename, testTree)
+module.exports = { testTree }

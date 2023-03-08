9d12c9fdc96aa2fe418a97d44abc0eaa9d7f1b1b
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2023-03-08 09:23:04 -0800

    checkpoint

--- browser/TrueBaseBrowserApp.js
+++ browser/TrueBaseBrowserApp.js
@@ -1,4 +1,4 @@
-class TrueBaseFrontEndApp {
+class TrueBaseBrowserApp {
   constructor() {
     window.app = this
   }
--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 #! /usr/bin/env node
 
-const { TestRacer } = require("jtree/products/TestRacer.js")
+const { TestRacer } = require("jtree/products/TestRacer.js");
 
 const testAll = async () => {
-  const fileTree = {}
-  let folders = `./code/TrueBase.test.js
-./code/TrueBaseServer.test.js
+  const fileTree = {};
+  let folders = `./server/TrueBase.test.js
+./server/TrueBaseServer.test.js
 ./tql/tql.test.js`
     .split("\n")
-    .forEach(file => (fileTree[file] = require(file).testTree))
-  const runner = new TestRacer(fileTree)
-  await runner.execute()
-  runner.finish()
-}
-testAll()
+    .forEach((file) => (fileTree[file] = require(file).testTree));
+  const runner = new TestRacer(fileTree);
+  await runner.execute();
+  runner.finish();
+};
+testAll();

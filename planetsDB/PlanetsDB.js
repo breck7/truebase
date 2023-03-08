3a07a229288ac8287a32d851b827dd0da6a250ed
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2023-03-08 09:23:04 -0800

    checkpoint

--- planetsDB/PlanetsDB.js
+++ planetsDB/PlanetsDB.js
@@ -1,23 +1,31 @@
 #!/usr/bin/env node
 
 // This is a demo TrueBase
-const path = require("path")
-const express = require("express")
+const path = require("path");
+const express = require("express");
 
-const { TrueBaseFolder } = require("../code/TrueBase.js")
-const { TrueBaseServer } = require("../code/TrueBaseServer.js")
+const { TrueBaseFolder } = require("../server/TrueBase.js");
+const { TrueBaseServer } = require("../server/TrueBaseServer.js");
 
-const ignoreFolder = path.join(__dirname, "..", "ignore")
-const truebaseFolder = path.join(__dirname)
-const frontendFolder = path.join(__dirname, "..", "frontEnd")
+const ignoreFolder = path.join(__dirname, "..", "ignore");
+const browserFolder = path.join(__dirname, "..", "browser");
+const truebaseFolder = path.join(__dirname);
 
 class PlanetsDB {
   start(port) {
-    const folder = new TrueBaseFolder().setDir(truebaseFolder).setGrammarDir(truebaseFolder)
-    const trueBaseServer = new TrueBaseServer(folder, ignoreFolder, truebaseFolder).initSearch().serveFolder(frontendFolder)
-    trueBaseServer.listen(port)
+    const folder = new TrueBaseFolder()
+      .setDir(truebaseFolder)
+      .setGrammarDir(truebaseFolder);
+    const trueBaseServer = new TrueBaseServer(
+      folder,
+      ignoreFolder,
+      truebaseFolder
+    )
+      .initSearch()
+      .serveFolder(browserFolder);
+    trueBaseServer.listen(port);
   }
 }
-if (!module.parent) new PlanetsDB().start(3333)
+if (!module.parent) new PlanetsDB().start(3333);
 
-module.exports = { PlanetsDB }
+module.exports = { PlanetsDB };

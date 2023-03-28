879c4a6e35d67617bcb186062a71f673f04c5d2d
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2023-03-28 13:11:10 -0500

    Start to make it clear that in TrueBase grammars everything revolves around columns

--- planetsDB/planetsdb.sql
+++ planetsDB/planetsdb.sql
@@ -1,9 +1,9 @@
 create table planetsdb (
  id TEXT NOT NULL PRIMARY KEY,
+ related TEXT,
  title TEXT,
  aka TEXT,
  wikipedia TEXT,
- related TEXT,
  hasLife INTEGER,
  surfaceGravity INTEGER,
  diameter INTEGER,
@@ -14,7 +14,7 @@ create table planetsdb (
  neighbors TEXT
 );
 
-INSERT INTO planetsdb (id,title,aka,wikipedia,related,hasLife,surfaceGravity,diameter,moons,age,description,yearsToOrbitSun,neighbors) VALUES ("earth","Earth","Planet Earth","Earth\npageViews 123","mars & venus",true,10,12756,1,"4500000000\nIt was only during the 19th century that geologists realized Earth's age was at least many millions of years.","It's where\nwe live.",1,"mars 110000000\nvenus 141000000");
+INSERT INTO planetsdb (id,related,title,aka,wikipedia,hasLife,surfaceGravity,diameter,moons,age,description,yearsToOrbitSun,neighbors) VALUES ("earth","mars & venus","Earth","Planet Earth","Earth\npageViews 123",true,10,12756,1,"4500000000\nIt was only during the 19th century that geologists realized Earth's age was at least many millions of years.","It's where\nwe live.",1,"mars 110000000\nvenus 141000000");
 INSERT INTO planetsdb (id,title,surfaceGravity,diameter,moons,yearsToOrbitSun) VALUES ("jupiter","Jupiter",25,142984,63,11.86);
 INSERT INTO planetsdb (id,title,surfaceGravity,diameter,moons,yearsToOrbitSun) VALUES ("mars","Mars",4,6794,2,1.881);
 INSERT INTO planetsdb (id,title,surfaceGravity,diameter,moons,yearsToOrbitSun) VALUES ("mercury","Mercury",4,4879,0,0.241);

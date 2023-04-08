112c63fbe9ec349bcec5b127b5db442095661187
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2023-04-08 11:43:01 -1000

    Init fixes

---
+++ cli.test.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+const fs = require("fs")
+const path = require("path")
+const { TestRacer } = require("jtree/products/TestRacer.js")
+const { Disk } = require("jtree/products/Disk.node.js")
+const { TrueBaseCli } = require("./cli.js")
+
+const tempDir = path.join(__dirname, "testBase")
+
+const testTree = {}
+
+testTree.all = equal => {
+  // Arrange
+  try {
+    const cli = new TrueBaseCli()
+    Disk.mkdir(tempDir)
+    cli.initCommand(tempDir)
+    cli.testCommand(tempDir)
+    equal(true, true, "no errors")
+  } catch (err) {
+    equal(false, true, "Expected no errors")
+    console.error(err)
+  }
+
+  // Cleanup
+  fs.rmSync(tempDir, { recursive: true })
+}
+
+if (!module.parent) TestRacer.testSingleFile(__filename, testTree)
+module.exports = { testTree }

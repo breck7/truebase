2d0f9e7a380ecba364f59c206ceb17c9fb6ebe37
breck7 <breck7@gmail.com>
2023-04-21 01:10:55 +0000

    Deploying to scroll-output from @ breck7/truebase@9e4522b58d6095bb03e94359520682541f574eed 🚀

--- server/TrueBase.ts
+++ server/TrueBase.ts
@@ -18,6 +18,7 @@ declare type parserDef = treeNode
 
 interface ColumnInterface {
   Column: string
+  ColumnLink: string
   Values: number
   Missing: number
   Coverage: string
@@ -516,7 +517,7 @@ class TrueBaseFolder extends TreeNode {
 
   get columnsCsvOutput() {
     const columnsMetadataTree = new TreeNode(this.columnDocumentation)
-    const columnMetadataColumnNames = ["Index", "Column", "Values", "Coverage", "Example", "Description", "Source", "SourceLink", "Definition", "DefinitionLink"]
+    const columnMetadataColumnNames = ["Index", "Column", "ColumnLink", "Values", "Coverage", "Example", "Description", "Source", "SourceLink", "Definition", "DefinitionLink"]
 
     const columnsCsv = columnsMetadataTree.toDelimited(",", columnMetadataColumnNames)
 
@@ -555,6 +556,8 @@ class TrueBaseFolder extends TreeNode {
       if (parserDef) Source = parserDef.getFrom("string sourceDomain")
       else Source = ""
 
+      const columnsToSelect = Column.includes("_") ? [Column, Column.split("_")[0]].join("+") : Column
+      const ColumnLink = `search.html?q=select+${columnsToSelect}%0D%0AnotMissing+${Column}%0D%0AsortBy+${Column}%0D%0Areverse`
       const sourceLocation = this.getFilePathAndLineNumberWhereParserIsDefined(colDefId)
       if (!sourceLocation.filePath) throw new Error(UserFacingErrorMessages.missingColumnSourceFile(sourceLocation.filePath))
 
@@ -563,6 +566,7 @@ class TrueBaseFolder extends TreeNode {
       const SourceLink = Source ? `https://${Source}` : ""
       return {
         Column,
+        ColumnLink,
         Values,
         Missing,
         Coverage: Math.round((100 * Values) / (Values + Missing)) + "%",
